7b18c6e8676e96512a28b2a39300a05224d8ae95
Lergin <lergin@gmx.de>
2017-11-07 22:21:09 +0100

    added an extra node for curr values

--- lib/updater/GamePlayersUpdater.ts
+++ lib/updater/GamePlayersUpdater.ts
@@ -4,9 +4,12 @@ import { CountUpdater } from "./CountUpdater";
 
 export class GamePlayersUpdater extends CountUpdater{
     private _interval: number;
+    private _currRef: admin.database.Reference;
 
     constructor(db: admin.database.Database) {
         super(db.ref("gamemodeStats"));
+
+        this._currRef = this._ref.child('curr');
 
         this._interval = 1000 * 60 * 60;
     }
@@ -24,6 +27,8 @@ export class GamePlayersUpdater extends CountUpdater{
             let players: number = await gameType.uniquePlayers(60*60*1000);
 
             this.sendNotification(players, gameType);
+
+            this._currRef.child(gameType.id).set(players);
 
             return this._ref.child(gameType.id).child(new Date().getTime().toString()).set(players);
         }));
--- lib/updater/UniquePlayerUpdater.ts
+++ lib/updater/UniquePlayerUpdater.ts
@@ -3,9 +3,12 @@ import { Server } from "hive-api";
 
 export class UniquePlayerUpdater extends CountUpdater {
   private _interval: number;
+  private _currRef: admin.database.Reference;
 
   constructor(db: admin.database.Database) {
     super(db.ref("uniquePlayers"), "uniquePlayers");
+
+    this._currRef = db.ref("uniquePlayersCurr");
 
     this._interval = 1000 * 60 * 5;
   }
@@ -21,6 +24,7 @@ export class UniquePlayerUpdater extends CountUpdater {
   async updateInfo() {
     return Server.uniquePlayers(this._interval).then(amount => {
         this.sendNotification(amount);
+        this._currRef.set(amount);
         return this._ref.child(new Date().getTime().toString()).set(amount);
       }
     );
