97b530807c4174bdb7ac015768c263584face8ca
Lergin <malte@laukoetter.eu>
2020-07-10 22:14:04 +0200

    Remove left over SKY data collection

--- lib/updater/AchievementUpdater.ts
+++ lib/updater/AchievementUpdater.ts
@@ -10,7 +10,6 @@ export class AchievementUpdater extends LeaderboardUpdater {
            GameTypes.HIDE,
            GameTypes.SP,
            GameTypes.TIMV,
-           GameTypes.SKY,
            GameTypes.DRAW,
            GameTypes.GRAV,
            GameTypes.BED,
